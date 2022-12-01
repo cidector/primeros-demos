cfcf9d9e055147655d831bd77ec9c8830b3ca84e
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 16:38:48 -0500

    se hizo cambios

--- bucket.tf
+++ bucket.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "tf-state"
-name     = "demo-00-a123f"
+name     = "terraform-03"
 location = "EU"
 force_destroy = false
 # Es recomendable activar el versing pra los state para poder hacer roll-back a una version anterior.
