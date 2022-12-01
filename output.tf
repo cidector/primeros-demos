7a5199f0945051f1d22afd0687d7673f3e03690f
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 13:46:17 -0500

    octava linea agregada

--- output.tf
+++ output.tf
@@ -6,3 +6,6 @@ output "google_storage_bucket" {
 output "google_compute_instance" {
     description = "compute instance name"
     value = google_compute_instance.default.name
+}
+
+output "google_compute_instance_ip{
