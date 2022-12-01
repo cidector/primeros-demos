1797e45d5337f4362e45b89daa1365b7142ddc89
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 12:27:14 -0500

    se acaba de agregar la ultima linea de provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,8 @@
 provider "google" {
     region = "europe-west1"
     zone   = "europe-west1-c"
+    #sustyituye por el project id por el que corresponda en tu caso.
+    project = "{PROJECT_ID}"
+    }
     
     
