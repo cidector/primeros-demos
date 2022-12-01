a0d09886c5d19bb07d54fb242b603f83a7bd86d6
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 14:35:42 -0500

    se esta agregando el ID

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "google" {
     region = "europe-west1"
     zone   = "europe-west1-c"
     #sustyituye por el project id por el que corresponda en tu caso.
-    project = "{PROJECT_ID}"
+    project = "axial-palace-370319"
     }
     
     
