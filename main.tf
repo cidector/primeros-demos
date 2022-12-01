356d3ed0910619b1b8fd1255809b3ce4f4b069d6
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 16:40:02 -0500

    se hizo cambio en name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 #este ejemplo crea una instancia de VM con debin 10 de tamañof1-micro en europe-wet1-c
 
 resource "google_compute_instance" "default" {
-    name    = "instance-vm-01"
+    name    = "instance-vm-03"
     machine_type ="f1-micro"
     zone    = "europe-west1-c"
     boot_disk {
