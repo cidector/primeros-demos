184589e844a3db9927c0633833434ceb644ddebc
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 13:49:24 -0500

    se agrego una ultima linea de codigo

--- output.tf
+++ output.tf
@@ -10,3 +10,5 @@ output "google_compute_instance" {
 
 output "google_compute_instance_ip{
     description = "compute instance network ip"
+    value = google_compute_instance.default.network_interface.0.network_ip
+}
