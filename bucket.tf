841fb380a70cf6b8f3297a73fb5e392ed1c8e30e
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 14:32:36 -0500

    se agrego todo

--- bucket.tf
+++ bucket.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "tf-state"
 name     = "{NOMBRE_BUCKET}"
 location = "EU"
-force_destroy = fasle
+force_destroy = false
 # Es recomendable activar el versing pra los state para poder hacer roll-back a una version anterior.
 uniform_bucket_level_access = true
 versing {
