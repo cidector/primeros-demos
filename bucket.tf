f3265c6dda3bc0045d070b735218c1138c8110e7
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 11:59:49 -0500

    agregando comentario en backet

--- bucket.tf
+++ bucket.tf
@@ -2,3 +2,4 @@ resource "google_storage_bucket" "tf-state"
 name     = "{NOMBRE_BUCKET}"
 location = "EU"
 force_destroy = fasle
+# Es recomendable activar el versing pra los state para poder hacer roll-back a una version anterior.
