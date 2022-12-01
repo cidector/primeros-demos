7f4ef2f7343a4cefc1864e313e870e1292fc017d
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 13:38:54 -0500

    se agrego cuarta linea

--- output.tf
+++ output.tf
@@ -2,3 +2,5 @@ output "google_storage_bucket" {
     description = "bucket used for storing terraform state"
     value = google_storage_bucket.tf-state.name
 }
+
+output "google_compute_instance" {
