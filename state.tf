bd753d14c28170cb6a88613551815b396bf90d23
Jose Anicama <telegamer2016@gmail.com>
2022-12-01 15:06:31 -0500

    se agregao cambios

--- state.tf
+++ state.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "gcs" {
         #sustituir el nombre del bucket.
-        backet = "demo-00-a123f"
+        bucket = "demo-00-a123f"
         prefix ="terraform/state/example-00/"
     }
 }
